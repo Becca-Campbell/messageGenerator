eaf484085bfd7843d4d6b7ac421ebd9677bdc814
Becca.Campbell <r.garvin.work@gmail.com>
2025-02-03 12:31:30 -0500

    Add message components

---
+++ messageGenerator.js
@@ -0,0 +1,3 @@
+let weather = ["rainy", "sunny", "windy", "hot", "cold", "stormy", "freezing"];
+let emotion = ["happy", "excited", "silly", "calm", "grumpy", "energetic", "curious"];
+let animal = ["elephant", "fox", "penguin", "squirrel", "dog", "cat", "rabbit"];
